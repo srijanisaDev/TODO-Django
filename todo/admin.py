f5b6b2ed71a51b0e5ff7cc17f7405c2ad80824f3
srijanisaDev <srijankumar770@gmail.com>
2026-05-04 13:34:10 +0530

    some changes

--- todo/admin.py
+++ todo/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Task
 
 # Register your models here.
-class TaskaAdmin(admin.ModelAdmin):
+class TaskAdmin(admin.ModelAdmin):
     list_display = ('task' , 'is_completed' , 'updated_at')
     search_fields = ('task',)
 
-admin.site.register(Task , TaskaAdmin)
+admin.site.register(Task , TaskAdmin)
